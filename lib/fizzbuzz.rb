51851704383ecaa1cadd33c907bbaaedd375ecdf
Jenny <jennybell42@gmail.com>
2021-04-19 11:35:50 +0100

    fizzbuzz.rb_passes_basic_tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,9 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if number % 5 == 0 && number % 3 == 0
+    return 'fizzbuzz'  
+  elsif number % 3 == 0
     return 'fizz'
   elsif number % 5 == 0
-    return 'buzz'
+    return 'buzz' 
   end
 end
